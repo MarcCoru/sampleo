5cc44ee2016409fa13bdb5f5acdb8fbf77771dcf
ale <acocac@gmail.com>
2018-06-06 23:52:45 +0100

    test two approaches raster

--- get_raster.py
+++ get_raster.py
@@ -42,7 +42,6 @@ pt_list = gj['features'][0]["geometry"]["coordinates"][0]
 # convert point list (wgs) to shapely geometry object
 #geom = shapely.geometry.Polygon(pt_list)
 geom = ee.Geometry.Polygon(pt_list)
-print(geom.getInfo()['coordinates'])
 
 # read basename name from geojson file
 name = os.path.basename(args.geojson).replace(".geojson","")
@@ -61,34 +60,91 @@ utm_ids = utm_ids.getInfo()
 collection = collection.filterMetadata('MGRS_TILE', 'starts_with', utm_ids[0])
 print(name + ' tile contains: ' + str(collection.size().getInfo()) + ' S2TOA images')
 
-# approach: export by date 
-# source: https://groups.google.com/forum/#!searchin/google-earth-engine-developers/imagecollection|sort:date/google-earth-engine-developers/XP_gsnwI8cI/-N1CVryYCwAJ
-# https://groups.google.com/forum/#!searchin/google-earth-engine-developers/distinct$20python%7Csort:date/google-earth-engine-developers/F34-_mIksZo/FhH9Nv0VCgAJ
-# create export tasks
-images = collection.limit(1)
+### approach 1: export by date(slow)
+### source: https://groups.google.com/forum/#!searchin/google-earth-engine-developers/imagecollection|sort:date/google-earth-engine-developers/XP_gsnwI8cI/-N1CVryYCwAJ
+### https://groups.google.com/forum/#!searchin/google-earth-engine-developers/distinct$20python%7Csort:date/google-earth-engine-developers/F34-_mIksZo/FhH9Nv0VCgAJ
+### create export tasks
+images = collection.limit(2)
+images = collection
 
-_size = collection.size().getInfo()
-img = collection.distinct('system:time_start')
+_size = images.size().getInfo()
+
+img = images.distinct('system:time_start')
+        
 img_list = img.toList(img.size(), 0)
 
+folder = "raster_240_bydate"
+
 for index in range(0, _size-1):
+
     img = ee.Image(img_list.get(int(index)))
 
-    doy = ee.String(img.date().getRelative('day', 'year').add(1)).getInfo()                                  
-    print(doy)
-                                 
+#    doy = ee.String(img.date().getRelative('day', 'year').add(1)).getInfo()                                  
+#    print(doy)
+                          
+    date = ee.String(img.date().format('YYYY-MM-dd')).getInfo()
+    
     task = 'image' + str(index)
     
-    task = ee.batch.Export.image.toCloudStorage(image = img, description=name + "_" + str(doy), 
-                                  scale = 10, fileFormat= 'TFRecord', region = geom.getInfo()['coordinates'], bucket='gs://sampleo/',
-                                  formatOptions = {'patchDimensions': [24, 24]})
+    task = ee.batch.Export.image.toCloudStorage(image = img, description=name + "_" + str(date), 
+                                                fileNamePrefix= folder + '/' + name + "_" + str(date),
+                                  scale = 10, fileFormat= 'TFRecord', region = geom.getInfo()['coordinates'], bucket='sampleo',
+                                  formatOptions = {'patchDimensions': [24, 24], 
+                                  'collapseBands': False, 'compressed': True})
 
     task.start() 
+    print('Done.', task.status())
+
     time.sleep(1)
 
     state=task.status()['state'] 
     while state in ['READY', 'RUNNING']:
         print(state + '...')
         state = task.status()['state']
-        time.sleep(1)
+#        time.sleep(1)
     print('Done.', task.status())
+#
+## approach 2: export TFrecords using neighborhoodToArray - return all NOBS by band (faster)
+#def kernelnb(img):
+#    return img.neighborhoodToArray(kernel_10m).sampleRegions(collection= tile_point)
+#
+##tile_point = ee.Feature(ee.Geometry.Point(geom.centroid().coordinates()))
+#tile_point = ee.Feature(ee.Geometry.Point(pt_list[4]))
+#
+##size = 24
+##weights = ee.List.repeat(ee.List.repeat(1, size), size)
+##kernel_10m = ee.Kernel.fixed(size, size, weights)
+####Sample pixels in the ImageCollection at these random points
+##values_10m = collection.map(kernelnb).flatten()
+##        
+##print(values_10m)
+#
+###create kernel
+##kernel_10m = ee.Kernel.square(11) #square kernell
+#size = 24
+#weights = ee.List.repeat(ee.List.repeat(1, size), size)
+#kernel_10m = ee.Kernel.fixed(size, size, weights)
+#
+###Sample pixels in the ImageCollection at these random points
+#values_10m = collection.map(kernelnb).flatten()
+##print(values_10m.size().getInfo())
+#
+#folder = "raster_240_all_kernelfixed"
+#
+#task= ee.batch.Export.table.toCloudStorage(
+#        collection= values_10m,
+#        description= name,
+#        folder= None,
+#        fileNamePrefix= folder + '/' + name + 'v4',
+#        fileFormat= 'TFRecord',
+#        bucket='sampleo')
+#        
+#try:
+#    task.start()
+#    #TODO: Implement log dump instead of print
+#    print("Processing year: ", folder,' and tile: ', name)
+#    time.sleep(15)
+#    print(task.status())
+#except Exception as str_error:
+#    print("Error in year: ", folder,' and tile: ', name)
+#    print("Error type equal to ", str_error)
