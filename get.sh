e9c36bb1be882235a5fe4d2a3049aa4220df0c04
Marc Rußwurm <marc.russwurm@tum.de>
2018-06-11 14:56:32 +0200

    implemented waiting for tasks to finis

--- get.sh
+++ get.sh
@@ -23,19 +23,22 @@ for id in $bavariaids; do
     fi
 
     # dummy check number of running tasks
+    max_num_tasks=1
     num_running=$(earthengine task list | grep RUNNING | wc -l)
-    echo $num_running
-    if (( $num_running > 2 )); then
-        echo wait
-    fi
+    while [ "$num_running" -gt "$max_num_tasks" ]
+    do
+        echo "waiting for $num_running GEE tasks to finish..."
+        sleep 5
+        num_running=$(earthengine task list | grep RUNNING | wc -l)
+    done
 
     already_downloaded_ids=$(gsutil ls gs://sampleo/tiles | grep -o [0-9]*)
 
     python get_geojson.py "from geegrid where id=$id" data/$id.geojson
 
-    python get_srdata.py --geojson data/$id.geojson -ts 2016-01-01 -te 2016-12-31 --collection "COPERNICUS/S2" -b 'sampleo' -f tiles/$id -r 10
-    python get_srdata.py --geojson data/$id.geojson -ts 2016-01-01 -te 2016-12-31 --collection "COPERNICUS/S2" -b 'sampleo' -f tiles/$id -r 20
-    python get_srdata.py --geojson data/$id.geojson -ts 2016-01-01 -te 2016-12-31 --collection "COPERNICUS/S2" -b 'sampleo' -f tiles/$id -r 60
+    python get_srdata.py --geojson data/$id.geojson -ts 2016-01-01 -te 2016-12-31 --collection "COPERNICUS/S2" -b 'sampleo' -f tiles/$id -r 10 > /dev/null &
+    python get_srdata.py --geojson data/$id.geojson -ts 2016-01-01 -te 2016-12-31 --collection "COPERNICUS/S2" -b 'sampleo' -f tiles/$id -r 20 > /dev/null &
+    python get_srdata.py --geojson data/$id.geojson -ts 2016-01-01 -te 2016-12-31 --collection "COPERNICUS/S2" -b 'sampleo' -f tiles/$id -r 60 > /dev/null &
 
     #python get_srdatapy.py 'data/$id.geojson' -ts 2016-01-01 -te 2016-12-31 --collection "LANDSAT/LC08/C01/T1" -f gs://sampleo/tiles/$id/LSraster30m.tfrecord -r 30
 
